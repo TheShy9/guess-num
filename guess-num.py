dd6e4fcf10067636684047e7b21b42b910d29ecd
chen xiao <1090021294@qq.com>
2019-08-29 09:43:08 +0800

    add count

--- guess-num.py
+++ guess-num.py
@@ -1,10 +1,13 @@
 import random
 r = random.randint(1,100)
+count = 0
 while True:
+	count += 1 #count = count + 1 的简写
 	guess = input('请猜测数字的大小： ')
 	guess = int(guess)
 	if guess == r:
 		print('终于猜对了')
+		print('这是你猜的第', count, '次')
 		break
 	else:
 		x = guess - r
@@ -12,4 +15,5 @@ while True:
 			print('比答案大')
 		else:
 			print('比答案小')
+	print('这是你猜的第', count, '次')
 		
